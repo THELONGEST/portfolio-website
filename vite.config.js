237f363f1a795f7b197fbd9cfb77b67fa6aef770
THELONGEST <truongdinhlong4@gmail.com>
2025-03-15 23:05:28 +0700

    Optimize Vite config for production build

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,23 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
-    minify: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          emailjs: ['@emailjs/browser']
+        }
+      }
+    }
   },
+  optimizeDeps: {
+    include: ['react', 'react-dom', '@emailjs/browser']
+  }
 })
